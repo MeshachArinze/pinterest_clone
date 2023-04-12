24bb464913629ce07cb724bc6f69908b9ef43e18
MeshachArinze <meshacharinze@gmail.com>
2023-04-12 21:50:02 +0100

    commit to repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+
+import "./App.css";
+import Header from "./components/Header";
+import Mainboard from "./components/Mainboard";
+import unsplash from "./api/unsplash";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pins, setNewPins] = useState([]);
+
+  const getImages = (term) => {
+    return unsplash.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+    });
+  };
+
+  const getNewPins = () => {
+    let promises = [];
+    let pinData = [];
+    let pins = [
+      "dogs",
+      "cars",
+      "tokyo",
+      "japan",
+      "beaches",
+      "cats",
+      "sunset",
+      "illustrations",
+      "coffee",
+      "desserts",
+      "christmas decor",
+    ];
+
+    pins.forEach((pinTerm) => {
+      promises.push(
+        getImages(pinTerm).then((res) => {
+          let results = res.data.results;
+          pinData = pinData.concat(results);
+          pinData.sort(function (a, b) {
+            return 0.5 - Math.random();
+          });
+        })
+      );
+    });
+
+    Promise.all(promises).then(() => {
+      setNewPins(pinData);
+    });
+  };
+
+  useEffect(() => {
+    getNewPins();
+  }, []);
+
+  const onSearchSubmit = (term) => {
+    getImages(term).then((res) => {
+      let results = res.data.results;
+      let newPins = [...results, ...pins];
+      newPins.sort(function (a, b) {
+        return 0.5 - Math.random();
+      });
+      setNewPins(newPins);
+    });
+  };
 
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Header onSubmit={onSearchSubmit} />
+      <Mainboard pins={pins} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
